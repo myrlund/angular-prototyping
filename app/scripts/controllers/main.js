e7b4c829b701bd6c98ff575869caaf7066d45692
Jonas Myrlund <myrlund@gmail.com>
2013-05-13 17:42:28 +0200

    Modified main controller.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,20 @@
 
 angular.module('PrototypingApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+
+    // Start off with some fancy words...
+    $scope.fancyWords = [
+      'Agile',
+      'Cloud',
+      'Platform'
     ];
+
+    // Add a fancy word!
+    $scope.addWord = function(){
+      if ($scope.newWord) {
+        $scope.fancyWords.push($scope.newWord);
+      }
+
+      $scope.newWord = "";
+    }
   });
