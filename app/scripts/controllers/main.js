ca5cc0fb19567c5341c92429054bfe2109a9947f
Jonas Myrlund <myrlund@gmail.com>
2013-05-14 11:47:29 +0200

    Firebase set up.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,24 +4,20 @@ angular.module('PrototypingApp')
   .controller('MainCtrl', ['$scope', 'angularFire', function ($scope, angularFire) {
 
     var url = 'https://comoyo-test.firebaseio.com/items';
+
     var promise = angularFire(url, $scope, 'items', []);
+
     promise.then(function(l) {
-      console.log(l);
+
+      // Add a fancy word!
+      $scope.addWord = function(){
+        if ($scope.newWord) {
+          $scope.items.push($scope.newWord);
+        }
+
+        $scope.newWord = "";
+      };
+
     });
 
-    // Start off with some fancy words...
-    $scope.fancyWords = [
-      'Agile',
-      'Cloud',
-      'Platform'
-    ];
-
-    // Add a fancy word!
-    $scope.addWord = function(){
-      if ($scope.newWord) {
-        $scope.fancyWords.push($scope.newWord);
-      }
-
-      $scope.newWord = "";
-    };
   }]);
